a4a7bdd6295f03b4a766066d9385889be2eae285
emregul <emre.gul@it-bureau.nl>
2024-04-05 01:54:56 +0200

    ci: install cypress ci

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '96exdm',
   defaultCommandTimeout: 6000,
   env:{
     url:"https://rahulshettyacademy.com"
